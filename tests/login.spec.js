2c185c162d9adc8e4a20b82b7a650331c2332c20
lgc2 <lgustavoccarvalho@gmail.com>
2026-07-26 18:08:46 -0300

    ci: add publish report steps and environment secrets

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,11 +1,17 @@
 import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const username = process.env.SAUCEDEMO_USERNAME;
+const password = process.env.SAUCEDEMO_PASSWORD;
 
 test('Should login successfully', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await expect(page.locator('.login_logo')).toHaveText('Swag Labs');
 
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="username"]').fill(username);
+  await page.locator('[data-test="password"]').fill(password);
   await page.locator('[data-test="login-button"]').click();
   await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
   await expect(page.locator('[data-test="title"]')).toHaveText('Products');
